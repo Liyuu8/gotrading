6618f39c8f5f285e61a4b6feadc49e68af1563a9
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2020-10-21 02:03:52 +0900

    Implement dfCandle info APIs

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -1,10 +1,14 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"gotrading/app/models"
 	"gotrading/config"
+	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 	"text/template"
 )
 
@@ -22,8 +26,73 @@ func viewChartHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// JSONError is ...
+type JSONError struct {
+	Error string `json:"error"`
+	Code  int    `json:"code"`
+}
+
+// APIError is ...
+func APIError(w http.ResponseWriter, errMessage string, code int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	jsonError, err := json.Marshal(JSONError{Error: errMessage, Code: code})
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(jsonError)
+}
+
+var apiValidPath = regexp.MustCompile("^/api/candle/$")
+
+func apiMakeHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := apiValidPath.FindStringSubmatch(r.URL.Path)
+		if len(m) == 0 {
+			APIError(w, "Not found", http.StatusNotFound)
+		}
+		fn(w, r)
+	}
+}
+
+func apiCandleHandler(w http.ResponseWriter, r *http.Request) {
+	productCode := r.URL.Query().Get("product_code")
+	if productCode == "" {
+		APIError(w, "No product_code param", http.StatusBadRequest)
+		return
+	}
+
+	strLimit := r.URL.Query().Get("limit")
+	limit, err := strconv.Atoi(strLimit)
+	if strLimit == "" || err != nil || limit < 0 || limit > 1000 {
+		limit = 1000
+	}
+
+	duration := r.URL.Query().Get("duration")
+	if duration == "" {
+		duration = "1m"
+	}
+	durationTime := config.BitflyerConfig.Durations[duration]
+
+	df, _ := models.GetAllCandle(productCode, durationTime, limit)
+
+	js, err := json.Marshal(df)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
 // StartWebServer is ...
 func StartWebServer() error {
+	http.HandleFunc("/api/candle/", apiMakeHandler(apiCandleHandler))
+	// http://localhost:8080/api/candle/?product_code=BTC_JPY
+	// http://localhost:8080/api/candle/?product_code=BTC_JPY&duration=1s
+	// http://localhost:8080/api/candle/?product_code=BTC_JPY&duration=1s&limit=1
+
 	http.HandleFunc("/chart/", viewChartHandler)
+	// http://localhost:8080/chart/
+
 	return http.ListenAndServe(fmt.Sprintf(":%d", config.BitflyerConfig.Port), nil)
 }
