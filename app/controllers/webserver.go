4bfbbbf749c9369f04be7929c6b8a9e3d1007f78
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2020-10-21 20:14:45 +0900

    Get dfCandle info by Ajax

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -12,15 +12,10 @@ import (
 	"text/template"
 )
 
-var templates = template.Must(template.ParseFiles("app/views/google.html"))
+var templates = template.Must(template.ParseFiles("app/views/chart.html"))
 
 func viewChartHandler(w http.ResponseWriter, r *http.Request) {
-	limit := 100
-	duration := "1m"
-	durationTime := config.BitflyerConfig.Durations[duration]
-	df, _ := models.GetAllCandle(config.BitflyerConfig.ProductCode, durationTime, limit)
-
-	err := templates.ExecuteTemplate(w, "google.html", df.Candles)
+	err := templates.ExecuteTemplate(w, "chart.html", nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
